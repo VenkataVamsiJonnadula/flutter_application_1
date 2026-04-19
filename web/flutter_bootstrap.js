506e92eceb33cd51b69d6e8527d866d2d8790b98
VenkataVamsiJonnadula <venkat.nov20@gmail.com>
2026-04-19 15:05:31 +0530

    update loader configuration for improved app initialization

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,12 +1,12 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-_flutter.loader.loadEntrypoint({
-  onEntrypointLoaded: async function (engineInitializer) {
-    let appRunner = await engineInitializer.initializeEngine({
-      canvasKitBaseUrl: "/canvaskit/"
-    });
-    
-    appRunner.runApp();
+_flutter.loader.load({
+  config: {
+    canvasKitBaseUrl: "/canvaskit/"
   },
+  onEntrypointLoaded: async function(engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    await appRunner.runApp();
+  }
 });
